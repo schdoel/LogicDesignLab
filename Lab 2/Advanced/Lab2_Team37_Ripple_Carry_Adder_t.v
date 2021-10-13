ddb35de54b7e28924d449131cf7d9f8eccf8d923
schdoel <85329289+schdoel@users.noreply.github.com>
2021-10-13 16:38:03 +0800

    Update Lab2_Team37_Ripple_Carry_Adder_t.v

--- Lab 2/Advanced/Lab2_Team37_Ripple_Carry_Adder_t.v
+++ Lab 2/Advanced/Lab2_Team37_Ripple_Carry_Adder_t.v
@@ -4,6 +4,54 @@ module Ripple_Carry_Adder_t(a, b, cin, cout, sum);
     reg cin = 1'b0;
     wire cout;
     wire [7:0] sum;
+
+    Ripple_Carry_Adder rca_i (
+        .a(a),
+        .b(b),
+        .cin(cin),
+        .cout(cout),
+        .sum(sum)
+    );
+
+    initial begin
+        repeat (2 ** 8) begin
+            #1 a = a + 8'b1; b = 8'b0;
+            Test();
+            #1 c0 = c0 + 1'b1;
+            Test();
+            repeat(2 ** 8)
+                #1 b = b + 8'b1;
+                Test();
+                #1 c0 = c0 + 1'b1;
+                Test();
+            end
+        end
+        #1 $finish;
+    end
+    
+    //utility task for testing
+    task Test;
+    begin
+        if({cout, sum}!==(a+b+cin)) begin
+            $display("[ERROR]");
+            $write("a:%d",a);
+            $write("b:%d",b);
+            $write("c0 (cin):%d",c8);
+            $write("c8 (cout):%d",c0);
+            $write("s (Sum):%d",s);
+            $display;
+        end
+    end
+    endtask
+endmodule
+
+/*
+module Ripple_Carry_Adder_t(a, b, cin, cout, sum);
+    reg [7:0] a = 8'b0;
+    reg [7:0] b = 8'b0;
+    reg cin = 1'b0;
+    wire cout;
+    wire [7:0] sum;
     // reg CLK = 1;
 
     Ripple_Carry_Adder rca_i (
@@ -15,26 +63,21 @@ module Ripple_Carry_Adder_t(a, b, cin, cout, sum);
     );
 
     initial begin
-        //   {a, b} = 8'b0;
-        //   repeat (2 ** 8) begin
-        //     @ (posedge CLK)
-        //       Test;
-        //     @ (negedge CLK)
-        //       {a, b} = {a, b} + 8'b1;
-        //   end
-        repeat (2 ** 8) begin
-            #5 a = a + 8'b1; b = 8'b0;
-            Test();
-            #5 c0 = c0 + 1'b1;
-            Test();
-            repeat(2 ** 8)
-                #5 b = b + 8'b1;
-                Test();
-                #5 c0 = c0 + 1'b1;
-                Test();
-            end
+        repeat (2 ** 4) begin
+            #1 {a,b} = {a,b} + 16'b1;
+            Test;
+            #1 cin = cin+1'b1;
+            Test;
         end
-        #5 $finish;
+        
+        repeat (2 ** 4) begin
+            #1 {b,a} = {b,a} + 16'b1;
+            Test;
+            #1 cin = cin+1'b1;
+            Test;
+        end
+
+        #1 $finish;
     end
 
 
@@ -54,3 +97,4 @@ module Ripple_Carry_Adder_t(a, b, cin, cout, sum);
     end
     endtask
 endmodule
+*/
